9889e610519e9663a2f3d3ca8b27f999e12d9eeb
administator <administator>
2016-06-18 15:01:57 +0800

    improve the page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,20 @@
 $(function () {
+	$('.start').on('click', function () {
+		if ($(this).html() === 'start') {
+			$('.imggroup').attr('class', 'imggroup');
+			$('.imggroup').show();
+			$(this).html('end');
+		}
+		else {
+			$('.imggroup').addClass('animated slideOutRight');
+			setTimeout(function () {
+				$('.imggroup').hide();
+			}, 500);
+			$(this).html('start');
+		}
+	})
+
+
 	$('.imgbox').on('mouseenter', function () {
 		$(this).attr('class', 'imgbox');
 		$(this).addClass('animated shake');
